0da782c624adf3de77d87a630a535a1977d6f6b9
Denice Deatrich <denice.deatrich@gmail.com>
2023-08-02 14:13:27 -0600

    Put some files in subdirectories and fix issues:
      - 'editorfiles' and 'md_templates' contain vim and markdown templates
      - fix usage string

--- generate-new-guide.sh
+++ generate-new-guide.sh
@@ -11,11 +11,11 @@
 # RETURNS:	0  - OK
 #		1  - FAIL
 #
-# EXAMPLE:	./generate-new-guide.sh -d ~/doc/my-project -f my-guide.md
+# EXAMPLE:	./generate-new-guide.sh -d ~/doc/my-project -t my-guide
 #
 # CHANGES:	$Log$
 # NOTE:		This script won't work if you have spaces in your directory
-#		or your filename.
+#		or your top file/project name.
 ###############################################################################
 #set -v		# debugging tools
 #set -x
@@ -26,10 +26,10 @@ PATH="/bin:/usr/bin"
 cmd=$(basename $0)
 dbg=""
 usagestr="
-USAGE:\t$cmd -d SOME_DIR -n FILE_NAME
+USAGE:\t$cmd -d SOME_DIR -t FILE_NAME
 WHERE:\tSOME_DIR\tspecifies a new, non-existent directory
-WHERE:\tFILE_NAME\tspecifies the name of a document ending in '.md'
-Example: ./$cmd -d ~/docs/how-to-guide -n How-to-guide.md"
+WHERE:\tFILE_NAME\tspecifies the name of the top document without '.md' ending
+Example: ./$cmd -d ~/docs/how-to-guide -t How-to-guide"
 
 function err_exit( ) {
   echo -e "$cmd: $1"
@@ -47,7 +47,7 @@ while [ $1 ] ; do
   case $1 in
     -h) usage ;; 
     -d) shift; dir="$1" ;; 
-    -f) shift; file="$1" ;; 
+    -t) shift; file="$1" ;; 
      *) usage;;
   esac
   shift
@@ -58,8 +58,8 @@ if [ "$dir" = "" -o "$file" = "" ] ; then
 fi
 
 echo "$file" | grep -q '.md$'
-if [ $? -ne 0 ] ; then
-  err_exit "File name '$file' must end in '.md'"
+if [ $? -eq 0 ] ; then
+  err_exit "Top filename '$file' must not end in '.md'"
 fi
 
 if [ -d "$dir" ] ; then
@@ -70,8 +70,12 @@ if [ ! -f Makefile ] ; then
   err_exit "You need to be in the original project directory for this to work"
 fi
 
-files="chapter-template.md custom-highlight.theme guide-template.md Makefile"
-files=$files" metadata.md shell.xml style.css template.htm template.latex"
+files="custom-highlight.theme metadata.md Makefile"
+files=$files" console.xml style.css template.htm template.latex"
+
+editorfiles="console.vim .vimrc.example"
+
+md_templates="guide-template.md chapter-template.md"
 
 #modfiles="Makefile"
 
@@ -81,19 +85,45 @@ for f in $files ; do
   fi
 done
 
-echo dir is $dir and file is $file
 ## now we can make the directory, and copy files into place
-mkdir "$dir"
+mkdir -p "$dir"
 if [ $? -ne 0 ] ; then
   err_exit "mkdir '$dir' failed"
 fi
 
+mkdir -p "$dir/editorfiles"
+if [ $? -ne 0 ] ; then
+  err_exit "mkdir '$dir/editorfiles' failed"
+fi
+
+mkdir -p "$dir/md_templates"
+if [ $? -ne 0 ] ; then
+  err_exit "mkdir '$dir/md_templates' failed"
+fi
+
 for f in $files ; do
   cp -p "$f" "$dir"/
+  if [ $? -ne 0 ] ; then
+    err_exit "copy of '$f' to $dir/ failed"
+  fi
 done
 
-## alter Makefile by changing value of 'TARG'
-sed -i "s/pandoc-markdown-guide.md/$file/" "$dir"/Makefile
+for f in $editorfiles ; do
+  cp -p "$f" "$dir/editorfiles"/
+  if [ $? -ne 0 ] ; then
+    err_exit "copy of '$f' to $dir/editorfiles/ failed"
+  fi
+done
+
+for f in $md_templates ; do
+  cp -p "$f" "$dir/md_templates"/
+  if [ $? -ne 0 ] ; then
+    err_exit "copy of '$f' to $dir/md_templates/ failed"
+  fi
+done
+
+## alter Makefile by changing value of 'PROJECT'
+sed -i "s/pandoc-markdown-guide/$file/" "$dir"/Makefile
 if [ $? -ne 0 ] ; then
   err_exit "Change to Makefile failed"
 fi
@@ -104,10 +134,11 @@ if [ $? -ne 0 ] ; then
   err_exit "Change to contents.txt failed"
 fi
 
-## copy guide-template.md to $file
-cp -p $dir/guide-template.md $dir/$file
+## copy guide-template.md to top file
+destfile=$file".md"
+cp -p $dir/md_templates/guide-template.md $dir/$destfile
 if [ $? -ne 0 ] ; then
-  err_exit "Failed to copy guide-template.md to $dir/$file"
+  err_exit "Failed to copy md_templates/guide-template.md to $dir/$destfile"
 fi
 
 echo "Done.  Do not forget to modify 'metadata.md'"
